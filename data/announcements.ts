b18abbc68ff8047f33fd46ab2663b4a7c10f390d
Pavithra <pavithra@Pavithras-MacBook-Air.local>
2026-03-18 17:00:54 +0530

    added mtech webinar announcement

--- data/announcements.ts
+++ data/announcements.ts
@@ -16,10 +16,10 @@ export const announcements: Announcements = [
     linkUrl: "https://admissions.iitmz.ac.in/",
   },
   {
-    activeTill: "2026-02-25",
+    activeTill: "2026-03-25",
     content:
-      "Join Our Upcoming Webinar on M.Tech. in DS & AI on 25 February'26 at 5PM to 6 PM IST | 2.30 PM - 3.30 PM EAT",
+      "Join Our Upcoming Webinar on M.Tech. in DS & AI on 25 March'26 at 7PM to 8 PM IST | 4.30 PM - 5.30 PM EAT",
     linkText: "Register Now",
-    linkUrl: "https://forms.gle/hvCfJJEPctYY8H2Z8",
+    linkUrl: "/",
   },
 ];

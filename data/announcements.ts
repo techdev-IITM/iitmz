1883de2a5420c322320718a1cbfa0bcc87100a0c
Pavithra <pavithra@Pavithras-MacBook-Air.local>
2026-04-08 11:19:15 +0530

    added webinar info

--- data/announcements.ts
+++ data/announcements.ts
@@ -11,15 +11,29 @@ export const announcements: Announcements = [
   {
     activeTill: "2026-04-30",
     content:
+      "To know more about the BS admissions process, download the brochure",
+    linkText: "BS Info Brochure",
+    linkUrl: "/IB_BS_2026_updated.pdf",
+  },
+  {
+    activeTill: "2026-04-30",
+    content:
+      "To know more about the M.Tech. admissions process, download the brochure",
+    linkText: "M.Tech. Info Brochure",
+    linkUrl: "/IB_MTech_2026.pdf",
+  },
+  {
+    activeTill: "2026-04-30",
+    content:
       "BS & M.Tech. Admissions for the academic year 2026–27 are open now",
     linkText: "Apply Now",
     linkUrl: "https://admissions.iitmz.ac.in/",
   },
   {
-    activeTill: "2026-04-04",
+    activeTill: "2026-04-11",
     content:
-      "Join Our Upcoming BS Webinar on 04 April'26 at 5PM to 6:30 PM IST",
+      "Join Our Upcoming BS webinar with Allen Career Institute on 11 April'26 at 5PM to 6:30 PM IST | 2:30 - 3:30 PM EAT",
     linkText: "Register Now",
-    linkUrl: "https://forms.gle/mEEYkAEr31XdeAFJ7",
+    linkUrl: "https://forms.gle/mEubr2vzZTZ34RS69",
   },
 ];

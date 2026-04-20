35ef3e0be9c1b9738137f732b97875280e707ed3
Pavithra <pavithra@Pavithras-MacBook-Air.local>
2026-04-20 14:03:18 +0530

    removed the mtech extended

--- data/announcements.ts
+++ data/announcements.ts
@@ -8,32 +8,31 @@ export interface Announcement {
 export type Announcements = Announcement[];
 
 export const announcements: Announcements = [
-  {
-    activeTill: "2026-04-21",
-    content:
-      "M.Tech. applications has been extended to Monday, 20 April 2026, until 12:30 PM IST (10:00 AM EAT)",
-    linkText: "Apply Now",
-    linkUrl: "https://admissions.iitmz.ac.in/mtechdsai",
-  },
   // {
-  //   activeTill: "2026-04-30",
+  //   activeTill: "2026-04-21",
   //   content:
-  //     "To know more about the BS admissions process, download the brochure",
-  //   linkText: "BS Info Brochure",
-  //   linkUrl: "/IB_BS_2026_updated.pdf",
-  // },
-  // {
-  //   activeTill: "2026-04-30",
-  //   content:
-  //     "To know more about the M.Tech. admissions process, download the brochure",
-  //   linkText: "M.Tech. Info Brochure",
-  //   linkUrl: "/IB_MTech_2026.pdf",
-  // },
-  // {
-  //   activeTill: "2026-04-30",
-  //   content:
-  //     "BS & M.Tech. Admissions for the academic year 2026–27 are open now",
+  //     "M.Tech. applications has been extended to Monday, 20 April 2026, until 12:30 PM IST (10:00 AM EAT)",
   //   linkText: "Apply Now",
-  //   linkUrl: "https://admissions.iitmz.ac.in/",
+  //   linkUrl: "https://admissions.iitmz.ac.in/mtechdsai",
   // },
+  {
+    activeTill: "2026-04-30",
+    content:
+      "To know more about the BS admissions process, download the brochure",
+    linkText: "BS Info Brochure",
+    linkUrl: "/IB_BS_2026_updated.pdf",
+  },
+  {
+    activeTill: "2026-04-30",
+    content:
+      "To know more about the M.Tech. admissions process, download the brochure",
+    linkText: "M.Tech. Info Brochure",
+    linkUrl: "/IB_MTech_2026.pdf",
+  },
+  {
+    activeTill: "2026-04-30",
+    content: "BS Admissions for the academic year 2026–27 are open now",
+    linkText: "Apply Now",
+    linkUrl: "https://admissions.iitmz.ac.in/",
+  },
 ];

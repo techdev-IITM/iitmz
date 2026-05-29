4b6a180078a8a0a32fe4ffb6b43653b136a86ec3
Pavithra <pavithra@Pavithras-MacBook-Air.local>
2026-05-29 11:55:21 +0530

    added curtain raiser details

--- data/news.ts
+++ data/news.ts
@@ -7,6 +7,13 @@ export interface News {
 
 export const news: News[] = [
   {
+    title: "CPE - Curtain Raiser Event",
+    content:
+      "The Curtain-Raiser event brings together industry leaders, policymakers, and educational institutions from sectors such as chemical and pharmaceutical processing, energy, water, mineral and food processing, and related domains. The program will feature keynote and invited talks highlighting the vision, motivation, and architecture of the initiative at IITMZ, along with perspectives from academia and industry experts. Interactive breakout sessions in the afternoon will focus on selected concentration areas, encouraging discussions on shaping the later stages of the BS curriculum to better align with emerging industry and societal needs.",
+    img: "/curtain.png",
+    linkURL: "https://forms.gle/R8ayAsbTJs1euXQg8",
+  },
+  {
     title: "Tutoring Session Cohort 3",
     content:
       "Students who complete their BS application get exclusive access to Screening Test Training Sessions at IIT Madras Zanzibar, featuring live sessions, curated practice sets, and expert guidance. These sessions help you clearly understand the syllabus, practice with real sample questions, and learn effective strategies for objective exams, along with the opportunity to interact directly with IITM students through live Q&A sessions.",
@@ -23,24 +30,10 @@ export const news: News[] = [
   },
   // {
   //   title:
-  //     "Inauguration of IITM Zanzibar Library and “India Corner” by the High Commissioner of India to Tanzania",
+  //     "On July 11, 2025, as IIT Madras celebrated its 62nd Convocation in Chennai, IITM Zanzibar hosted its first-ever Degree Distribution Ceremony.",
   //   content:
-  //     "The High Commissioner of India to Tanzania, His Excellency Bishwadip Dey, officially inaugurated the IITM Zanzibar Library and the “India Corner” at the IITM Zanzibar campus. This marks yet another milestone in strengthening educational and cultural ties between India and Tanzania.",
-  //   img: "/india-corner.jpg",
-  //   linkURL: "https://www.instagram.com/p/DQJn54eDcKB/?img_index=5",
+  //     "This milestone marks the graduation of the inaugural MTech Data Science & AI cohort at our Zanzibar campus—a proud reflection of IIT Madras’s vision for global education and a shared legacy of excellence across continents. It also highlights the growing international collaboration in higher education and IIT Madras’s pioneering achievement in establishing India’s first offshore IIT campus.",
+  //   img: "/degree-distribution-ceremony.jpeg",
+  //   linkURL: "https://www.iitmz.ac.in/campus-life",
   // },
-  // {
-  //   title: "Zanzibar Innovation Day - October 4, 2025",
-  //   content: "IITM Zanzibar in collaboration with IITM Global will host Zanzibar Innovation Day on October 4, 2025, bringing together innovators, entrepreneurs, and industry leaders to celebrate creativity and entrepreneurship. The event will feature keynote talks, panel discussions, and interactive sessions designed to inspire collaboration and knowledge sharing. Emerging startups will present their innovative solutions, engaging directly with mentors and investors, showcasing the vibrant startup ecosystem in Zanzibar and the region. Zanzibar Innovation Day underscores IITM Zanzibar’s commitment to fostering innovation, nurturing talent, and building bridges between ideas and opportunities.",
-  //   img: "/innovation-day.jpeg",
-  //   linkURL: "https://drive.google.com/file/d/1Lcr-yCvS2VBqrqbyPh8JYrXrfx201eeJ/view?usp=sharing",
-  // },
-  {
-    title:
-      "On July 11, 2025, as IIT Madras celebrated its 62nd Convocation in Chennai, IITM Zanzibar hosted its first-ever Degree Distribution Ceremony.",
-    content:
-      "This milestone marks the graduation of the inaugural MTech Data Science & AI cohort at our Zanzibar campus—a proud reflection of IIT Madras’s vision for global education and a shared legacy of excellence across continents. It also highlights the growing international collaboration in higher education and IIT Madras’s pioneering achievement in establishing India’s first offshore IIT campus.",
-    img: "/degree-distribution-ceremony.jpeg",
-    linkURL: "https://www.iitmz.ac.in/campus-life",
-  },
 ];

291bc28db4ab6dd4a41ba9dd951d8d1e9ffa77ee
Pavithra <pavithra@Pavithras-MacBook-Air.local>
2026-08-16 17:53:09 +0530

    Update governing council board members list

--- components/governing-council.tsx
+++ components/governing-council.tsx
@@ -3,52 +3,16 @@ import Img from "./image";
 
 export const governanceBoardMember = [
   {
-    name: "Prof. Prabhu Rajagopal",
-    title:
-      "Director-in-Charge, Dean of the School of Engineering & Science, IITMZ",
-    img: "/prajagopal.png",
-  },
-  {
     name: "Prof. Mohammed Ali Sheikh",
     title:
       "Chairperson, Deputy Permanent Secretary (Fisheries), Ministry of Livestock and Fisheries, United Republic of Tanzania",
     img: "/council/Ali-Sheikh.png",
   },
   {
-    name: "Prof. Preeti Aghalayam",
-    title:
-      "Former Director-in-Charge, IITMZ, Dean (Office of Global Engagement), IIT Madras",
-    img: "/preeti.jpg",
-  },
-  {
-    name: "Prof. Raghunathan Rengaswamy",
-    title:
-      "Prof., Department Chemical Engg. IIT Madras , Board Member IITM Global",
-    img: "/prof-raghu.jpg",
-  },
-
-  {
     name: "Prof. Hamed R. H. Hikmany",
     title:
       "Principal, Zanzibar School of Health; Chairman, Board of Directors Zanzibar Revenue Authority(ZRA); Chairman, Council of the State University of Zanzibar (SUZA)",
     img: "/council/Hamed.png",
-  },
-  {
-    name: "Prof. Ligy Philip",
-    title:
-      "Nita and KG Ganapathy Institute Chair Professor, Department of Civil Engineering, IIT Madras",
-    img: "/ligy-1.png",
-  },
-  {
-    name: "Dr. Maryam Jaffar Ismail",
-    title:
-      "Assistant Director, Department of Higher Education ( Coordination ), Ministry of Education, Science and Technology",
-    img: "/council/maryam.png",
-  },
-  {
-    name: "Prof. Elisante Ole Gabriel",
-    title: "Tanzania Chief Court Administrator",
-    img: "/council/elisante-ole.png",
   },
   {
     name: "Prof. Maulilio John Kipanyula",
@@ -57,15 +21,21 @@ export const governanceBoardMember = [
     img: "/council/Maulilio.png",
   },
   {
-    name: "Prof. Said Ally Mohamed",
-    title: "Executive Secretary - National Examination Council Tanzania(NECTA)",
-    img: "/council/Said-Ally.png",
+    name: "Prof. Elisante Ole Gabriel",
+    title: "Tanzania Chief Court Administrator",
+    img: "/council/elisante-ole.png",
   },
   {
-    name: "Dr. Pramath Sinha",
+    name: "Prof. Abdi Talib Abdalla",
     title:
-      "Founder & Chairperson, Board of Trustees, Ashoka University; Founding Senior Partner, Jetri",
-    img: "/council/pramath-sinha.png",
+      "Deputy Vice Chancellor (Academic Affairs, Research and Consultancy), The State University of Zanzibar (SUZA)",
+    img: "/council/abdi-talib.png",
+  },
+  {
+    name: "Dr. Maryam Jaffar Ismail",
+    title:
+      "Assistant Director, Department of Higher Education ( Coordination ), Ministry of Education, Science and Technology",
+    img: "/council/maryam.png",
   },
   {
     name: "Prof. Ravindra Gettu",
@@ -74,10 +44,54 @@ export const governanceBoardMember = [
     img: "/council/Ravindra-Gettu.png",
   },
   {
-    name: "Prof. Abdi Talib Abdalla",
+    name: "Dr. Pramath Sinha",
     title:
-      "Deputy Vice Chancellor (Academic Affairs, Research and Consultancy), The State University of Zanzibar (SUZA)",
-    img: "/council/abdi-talib.png",
+      "Founder & Chairperson, Board of Trustees, Ashoka University; Founding Senior Partner, Jetri",
+    img: "/council/pramath-sinha.png",
+  },
+  {
+    name: "Prof. Said Ally Mohamed",
+    title: "Executive Secretary - National Examination Council Tanzania(NECTA)",
+    img: "/council/Said-Ally.png",
+  },
+  {
+    name: "Prof. Raghunathan Rengaswamy",
+    title:
+      "Prof., Department Chemical Engg. IIT Madras , Board Member IITM Global",
+    img: "/prof-raghu.jpg",
+  },
+  {
+    name: "Prof. Ligy Philip",
+    title:
+      "Nita and KG Ganapathy Institute Chair Professor, Department of Civil Engineering, IIT Madras",
+    img: "/ligy-1.png",
+  },
+  {
+    name: "Prof. Preeti Aghalayam",
+    title:
+      "Former Director-in-Charge, IITMZ, Dean (Office of Global Engagement), IIT Madras",
+    img: "/preeti.jpg",
+  },
+  {
+    name: "Prof. K. Suresh Akkihebbal",
+    title: "Vice President and Professor-in-Charge",
+    img: "/Prof-Suresh-Akkihebbal.jpg",
+  },
+  {
+    name: "Dr. Idrissa Said Amour",
+    title: "Chief Administrator",
+    img: "/staff/Idrissa Said Amour-1.jpg",
+  },
+  {
+    name: "Mr. Abdulrahman Ali Hassan",
+    title: "Chief Liaison Officer",
+    img: "/staff/abdulrahman-ali-hassan.png",
+  },
+  {
+    name: "Prof. Prabhu Rajagopal",
+    title:
+      "Director-in-Charge, Dean of the School of Engineering & Science, IITMZ",
+    img: "/prajagopal.png",
   },
 ];
 

a7267b8275c15834652af22349fe4becb9e40b1f
Pavithra <pavithra@Pavithras-MacBook-Air.local>
2026-08-24 12:37:47 +0530

    Update academic calendar for 2026-2027

--- data/calendar.ts
+++ data/calendar.ts
@@ -1,26 +1,26 @@
 export const academicCalendarData = [
   {
-    title: "Odd Semester: BS 5th Semester (Oct 2025 – Jan 2026)",
+    title: "Odd Semester: BS 5th Semester (Sep 2026 – Jan 2027)",
     link: "/Odd-Semester_BS-5th-Sem.pdf",
   },
   {
-    title: "Odd Semester: All others (Oct 2025 – Feb 2026)",
+    title: "Odd Semester: All others (Oct 2026 – Feb 2027)",
     link: "/odd-sem-all-others.pdf",
   },
   {
     title: "Even Semester: (Mar 2026 – Jul 2026)",
     link: "/Even-Semester.pdf",
   },
-  {
-    title: "Feb 2025 - Jul 2025",
-    link: "/AcademicCalendar-February-2025-July-2025.pdf",
-  },
-  {
-    title: "Oct 2024 - Feb 2025",
-    link: "/AcademicCalendar-October-2024-February-2025.pdf",
-  },
-  {
-    title: "March 2024 - July 2024",
-    link: "/AcademicCalendarMar-July2024.pdf",
-  },
+  // {
+  //   title: "Feb 2025 - Jul 2025",
+  //   link: "/AcademicCalendar-February-2025-July-2025.pdf",
+  // },
+  // {
+  //   title: "Oct 2024 - Feb 2025",
+  //   link: "/AcademicCalendar-October-2024-February-2025.pdf",
+  // },
+  // {
+  //   title: "March 2024 - July 2024",
+  //   link: "/AcademicCalendarMar-July2024.pdf",
+  // },
 ];

3a831cfe492a36fcf4794750de986960c7495a70
Pavithra <pavithra@Pavithras-MacBook-Air.local>
2026-09-10 14:54:23 +0530

    updated active till date for ITE Program

--- data/announcements.ts
+++ data/announcements.ts
@@ -16,7 +16,7 @@ export const announcements: Announcements = [
   //   linkUrl: "https://admissions.iitmz.ac.in/mtechdsai",
   // },
   {
-    activeTill: "2026-09-12",
+    activeTill: "2026-09-15",
     content: "International Teaching Experience (ITE) Program 2026_Sem2 applications open",
     linkText: "view website",
     linkUrl: "https://ge.iitm.ac.in/iie/ite-program",
